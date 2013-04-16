09fcdfa786a16fe608bdae5ea58c0905ea3486a4
MathieuPedrero <mathieu.pedrero@centraliens-lille.org>
2013-04-16 09:42:59 +0200

    adding id generator to model

--- eclihand/model-postgres-impl/src/main/java/com/pedrero/eclihand/model/domain/impl/DataObjectImpl.java
+++ eclihand/model-postgres-impl/src/main/java/com/pedrero/eclihand/model/domain/impl/DataObjectImpl.java
@@ -2,9 +2,12 @@ package com.pedrero.eclihand.model.domain.impl;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.pedrero.eclihand.model.domain.DataObject;
@@ -12,10 +15,12 @@ import com.pedrero.eclihand.model.domain.DataObject;
 @Entity
 @Table(name = "OBJ_OBJECT")
 @Inheritance(strategy = InheritanceType.JOINED)
+@SequenceGenerator(name = "OBJ_OBJECT_ID_SEQ", sequenceName = "OBJ_OBJECT_ID_SEQ", allocationSize = 1)
 public class DataObjectImpl implements DataObject {
 
 	@Id
 	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "OBJ_OBJECT_ID_SEQ")
 	private Long id;
 
 	@Column(name = "OBJ_INDEX")
