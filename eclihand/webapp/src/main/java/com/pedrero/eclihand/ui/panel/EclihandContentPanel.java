54514657f60fc9a02b70534adc688c192f3dc071
MathieuPedrero <mathieu.pedrero@centraliens-lille.org>
2013-11-04 21:47:22 +0100

    panel removal

---
+++ eclihand/webapp/src/main/java/com/pedrero/eclihand/ui/panel/EclihandContentPanel.java
@@ -0,0 +1,15 @@
+package com.pedrero.eclihand.ui.panel;
+
+import org.springframework.stereotype.Component;
+
+import com.vaadin.ui.VerticalLayout;
+
+@Component(value = "contentPanel")
+public class EclihandContentPanel extends VerticalLayout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5871025577106661499L;
+
+}
