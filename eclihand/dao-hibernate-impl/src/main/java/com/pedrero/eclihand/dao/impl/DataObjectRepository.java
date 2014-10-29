73d043714a08e3d328b197c2898509cb9f032585
Mathieu Pedrero <mathieu.pedrero@centraliens-lille.org>
2014-10-29 17:10:23 +0100

    rest server

--- eclihand/dao-hibernate-impl/src/main/java/com/pedrero/eclihand/dao/impl/DataObjectRepository.java
+++ eclihand/dao-hibernate-impl/src/main/java/com/pedrero/eclihand/dao/impl/DataObjectRepository.java
@@ -1,11 +1,13 @@
 package com.pedrero.eclihand.dao.impl;
 
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.pedrero.eclihand.dao.DataObjectDao;
 import com.pedrero.eclihand.dao.request.PageableParam;
 import com.pedrero.eclihand.model.domain.DataObject;
 
+@NoRepositoryBean
 public interface DataObjectRepository<T extends DataObject, U extends T> extends DataObjectDao<T>,
 		PagingAndSortingRepository<U, Long> {
 
