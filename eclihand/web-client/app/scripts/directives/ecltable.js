362be563b9cb3a8f47453dbf91eeb9302bc99172
Mathieu Pedrero <mathieu.pedrero@centraliens-lille.org>
2015-05-06 22:52:11 +0200

    generic table element

--- eclihand/web-client/app/scripts/directives/ecltable.js
+++ eclihand/web-client/app/scripts/directives/ecltable.js
@@ -7,41 +7,62 @@
  * # eclTable
  */
 angular.module('webClientApp')
-  .directive('eclTable', function () {
+  .directive('eclTable', function() {
     return {
-		templateUrl: 'views/data-table.html',
-		replace: true,
-        transclude: true,
-		restrict: 'E',
-		scope: {id: '=id', source: '=tableSource', message: '=tableMessage'},
-		
-        controller: ['$scope',function($scope){
-			$scope.columns = [];
-			var totalNumber;
-			$scope.page = 1;
-			$scope.pageSize = 10;
-			$scope.pageSizeChoices = [5,10,20];
-			
-			$scope.translationData={pageSize:$scope.pageSize};
-			
-			this.addColumn = function(column){
-				$scope.columns.push(column);
-			};
-			
-			this.lastPage = function(){
-				return $scope.source.length;
-			};
-				
-			$scope.selectPageSize = function(pageSize){
-				$scope.pageSize = pageSize;
-				$scope.translationData.pageSize = pageSize;
-            };
-				
-			$scope.directPaginationButtons = function(currentPage){
-				var pages = [];
-				var surroundingButtonsNumber = 2;
-				for (i=Math.max(1,currentPage-2);i<=Math.min()
-            };
-        }]
+      templateUrl: 'views/data-table.html',
+      replace: true,
+      transclude: true,
+      restrict: 'E',
+      scope: {
+        id: '=id',
+        source: '=tableSource',
+        message: '=tableMessage'
+      },
+
+      controller: ['$scope',
+        function($scope) {
+          $scope.columns = [];
+          $scope.page = 1;
+          $scope.pageSize = 10;
+          $scope.translationData = {
+            pageSize: $scope.pageSize
+          };
+          $scope.pageSizeChoices = [5, 10, 20];
+
+          var feedLastPage = function() {
+            $scope.lastPage = Math.max(0, 1 + Math.floor(($scope.source.length - 1) / $scope.pageSize));
+
+            console.log('last page is : ' + $scope.lastPage);
+          };
+
+          feedLastPage();
+
+          var computeDirectPaginationButtons = function() {
+            var pages = [];
+            var surroundingButtonsNumber = 2;
+            for (var i = Math.max(1, $scope.page - surroundingButtonsNumber); i <= Math.min($scope.lastPage, $scope.page + surroundingButtonsNumber); i++) {
+              pages.push(i);
+            }
+            $scope.directPaginationButtons = pages;
+          };
+
+          computeDirectPaginationButtons();
+
+          $scope.selectPage = function(selectedPage) {
+            $scope.page = selectedPage;
+            feedLastPage();
+            computeDirectPaginationButtons();
+          }
+
+          this.addColumn = function(column) {
+            $scope.columns.push(column);
+          };
+
+          $scope.selectPageSize = function(pageSize) {
+            $scope.pageSize = pageSize;
+            $scope.translationData.pageSize = pageSize;
+          };
+        }
+      ]
     };
   });
