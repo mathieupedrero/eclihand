7f19ab04b7e18a85b792bf3da113abe6496bd66e
Mathieu Pedrero <mathieu.pedrero@centraliens-lille.org>
2015-11-25 21:47:07 +0100

    error handling

--- eclihand/rest-server/src/main/java/com/pedrero/eclihand/rest/EclihandResponseEntityExceptionHandler.java
+++ eclihand/rest-server/src/main/java/com/pedrero/eclihand/rest/EclihandResponseEntityExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.pedrero.eclihand.rest;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +19,8 @@ import com.pedrero.eclihand.model.exception.EclihandRuntimeException;
 @ControllerAdvice
 public class EclihandResponseEntityExceptionHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(EclihandResponseEntityExceptionHandler.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(EclihandResponseEntityExceptionHandler.class);
 
 	@ExceptionHandler(value = EclihandRuntimeException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -30,8 +34,18 @@ public class EclihandResponseEntityExceptionHandler {
 	@ResponseBody
 	public EclihandMessage handleException(MethodArgumentNotValidException ex) {
 		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("Field validation exception {}", ex.getBindingResult().getAllErrors().get(0)
-					.getDefaultMessage(), ex);
+			LOGGER.debug("Field validation exception ");
+			BindingResult bindingResult = ex.getBindingResult();
+			LOGGER.debug("\t\t Error Default Message [{}] ", bindingResult
+					.getAllErrors().get(0).getDefaultMessage());
+			LOGGER.debug("\t\t Error Default code [{}] ", bindingResult
+					.getAllErrors().get(0).getCode());
+			LOGGER.debug(
+					"\t\t Error codes [{}] ",
+					Arrays.asList(bindingResult.getAllErrors().get(0)
+							.getCodes()));
+			LOGGER.debug("\t\t Object Name [{}] ", bindingResult.getAllErrors()
+					.get(0).getObjectName());
 			LocaleContextHolder.getLocale();
 		}
 		return null;
